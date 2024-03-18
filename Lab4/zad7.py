5c0d5a496c4a42f6aaf5b9a6f3020aa53b0af50e
Max Piotrowicz <61293187+WhiterPL@users.noreply.github.com>
2024-03-18 15:27:16 +0100

    Lab 4, zad 7

---
+++ Lab4/zad7.py
@@ -0,0 +1,47 @@
+import socket, select
+
+host = '127.0.0.1'
+port = 2901
+
+clients = []
+
+srv_socket = socket.socket()
+
+try:
+    srv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    srv_socket.bind((host, port))
+    srv_socket.listen(10)
+    clients.append(srv_socket)
+except:
+    print("Nieudane otworzenie socketa")
+
+print(f'Nasłuchiwanie połączeń pod adresem {host}:{port}')
+
+while True:
+
+    read_sockets, write_sockets, error_sockets = select.select(clients, [], [])
+
+    for sock in clients:
+
+        if sock == srv_socket:
+            client_socket, client_address = srv_socket.accept()
+            clients.append(client_socket)
+            print(f'Nowe połączenie spod adresu {client_address}')
+
+        else:
+            try:
+                data = sock.recv(1024)
+
+                if data:
+                    data = data.decode()
+
+                    if (len(data) <= 20):
+                        # Odesłanie danych klientowi
+                        sock.send(data)
+            except:
+                sock.close()
+                clients.remove(sock)
+                print("Rozłączon klienta...")
+                continue
+
+srv_socket.close()
