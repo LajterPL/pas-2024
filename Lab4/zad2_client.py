8c0aff2593132b995b4261a96d36a25ab187fd48
Max Piotrowicz <61293187+WhiterPL@users.noreply.github.com>
2024-03-18 14:44:13 +0100

    Lab 4, zad 2

---
+++ Lab4/zad2_client.py
@@ -0,0 +1,15 @@
+import socket
+
+host = '127.0.0.1'
+port = 2901
+
+with socket.socket() as s:
+    try:
+        s.settimeout(5)
+        s.connect((host, port))
+        s.send("Test".encode())
+        data = s.recv(1024).decode()
+        print(data)
+        s.close()
+    except TimeoutError:
+        print("Połączenie nieudane")
