3ae161b9c488d2f5404fd1c3dd35478b3719c900
Max Piotrowicz <61293187+WhiterPL@users.noreply.github.com>
2024-05-20 12:23:42 +0200

    zad 2

--- Lab10/zad2.py
+++ Lab10/zad2.py
@@ -4,20 +4,31 @@ import ssl
 host = "echo.websocket.org"
 port = 443
 
+
 class WebSocketPacket:
     def __init__(self, opcode, length, payload):
         self.opcode = opcode
         self.length = length
         self.payload = payload
-    def get_bytes(self) -> bytearray:
-        frame = bytearray()
 
-        frame.append(int('1000', 2))
-        frame.append(self.opcode)
-        frame.append(0)
+    def get_bytes(self) -> bytearray:
+        frame = ''
+
+        frame += '1' # FIN
+        frame += '000' # RSV
+        frame += bin(self.opcode)[2:].zfill(4) # OPCODE
+        frame += '1' # MASK
 
         if self.length <= 125:
-            
+            frame += bin(self.length)[2:].zfill(7)
+            frame = int(frame, 2).to_bytes(2, 'big')
+        elif self.length <= 131071:
+            frame += bin(126)[2:].zfill(7)
+            frame += bin(self.length)[2:].zfill(16)
+            frame = int(frame, 2).to_bytes(4, 'big')
+
+        frame += int(0).to_bytes(4, 'big')
+        frame += self.payload
 
         return frame
 
@@ -27,13 +38,27 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     s = ssl.SSLContext(protocol=ssl.PROTOCOL_SSLv23).wrap_socket(s, server_hostname=host)
 
+    websocket_frame = WebSocketPacket(1, len('hello world'.encode()), 'hello world'.encode())
+
+    print(websocket_frame.get_bytes())
+
     try:
         s.connect((host, port))
-        s.send(f'GET / HTTP/1.1\r\nHost: {host}\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\n\r\n'.encode())
-
+        s.send(
+            f'GET / HTTP/1.1\r\nHost: {host}\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\n\r\n'.encode())
 
         data = s.recv(2048)
         print(data.decode())
+
+        data = s.recv(2048)
+        print(data)
+
+        s.send(websocket_frame.get_bytes())
+
+        data = s.recv(2048)
+        print(data)
+
+        print("Wiadomość wysłana")
 
 
     except TimeoutError:
